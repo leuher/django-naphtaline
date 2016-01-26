1c3571aba37ba9f298f08de0aee96d5b4539f591
VirtualTam <virtualtam@flibidi.net>
2016-01-26 21:06:47 +0100

    lint: customize pylint settings, partially fix errors
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- demo/urls.py
+++ demo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+# pylint: disable=invalid-name
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
